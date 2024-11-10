aecc56de59e029b71216c7f154d46b63823fb856
Monica Zhang <108095408+monica410@users.noreply.github.com>
2024-11-09 23:48:27 -0800

    files

---
+++ src/Mancala.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Mancala {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Mancala");
+        f.setBounds(100, 0, 1200, 800);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+
+    }
+}
