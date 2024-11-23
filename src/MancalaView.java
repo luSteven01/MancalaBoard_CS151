93917944d6305281356d3bca2e4f1b5e5bc07561
luSteven01 <steven.lu01@sjsu.edu>
2024-11-23 15:20:24 -0800

    started making pits and stones (work in progress)

--- src/MancalaView.java
+++ src/MancalaView.java
@@ -1,42 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class MancalaView extends JPanel {
 
-    private MancalaModel model;
-    private BoardPatternStrategy boardPattern;
+    private final MancalaModel model;
+    private BoardPanel boardPanel;
 
     public MancalaView(MancalaModel model) {
-        super();
         this.model = model;
 
         model.addChangeListener(e -> {
-            boardPattern = model.getBoardPattern();
-            repaint();
+            BoardPatternStrategy boardPattern = model.getBoardPattern();
+            setBackground(boardPattern.color()); // Update background color
+            boardPanel.updatePattern(boardPattern); // Update pits dynamically
         });
 
-        boardPattern = model.getBoardPattern();
-
-        JLabel label = new JLabel("mancala view");
-
-        this.add(label);
-
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-
-        if (boardPattern != null) {
-            g2.setColor(boardPattern.color());
-            g2.fillRect(0, 0, getWidth(), getHeight());
-
-
-            g2.setColor(boardPattern.pitColor());
-            for (int i = 0; i < 6; i++) {
-                g2.fillOval(50 + i * 100, 100, 80, 80);
-            }
-        }
+        setLayout(new BorderLayout());
+        BoardPatternStrategy initialPattern = model.getBoardPattern();
+        boardPanel = new BoardPanel(model.getPits(), model.getMancalaA(), model.getMancalaB(), initialPattern);
+        add(boardPanel, BorderLayout.CENTER);
     }
 }
+
--- src/Pit.java
+++ src/Pit.java
@@ -1,22 +1,51 @@
+import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
-public class Pit {
+public class Pit extends JPanel {
 
-    private int stones;
-    private Rectangle bound;
+    private static final int PIT_SIZE = 60; // Fixed size for pits
+    private int stones; // Number of stones in the pit
+    private final ArrayList<JLabel> stoneIcons;
 
-    public Pit() {
+    public Pit(int initialStones) {
+        this.stones = initialStones;
+        this.stoneIcons = new ArrayList<>();
 
+        setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5)); // Align stones neatly
+        setPreferredSize(new Dimension(PIT_SIZE, PIT_SIZE));
+        setMaximumSize(new Dimension(PIT_SIZE, PIT_SIZE));
+        setMinimumSize(new Dimension(PIT_SIZE, PIT_SIZE));
+        setBackground(Color.LIGHT_GRAY); // Default pit background
+        updateStones(initialStones);
     }
 
-    public void increment() {
-        stones++;
+    public void addStones(int count) {
+        stones += count;
+        updateStones(stones);
     }
 
-    public int remove() {
-        int stones = this.stones;
-        this.stones = 0;
-        return stones;
+    public int removeStones() {
+        int removed = stones;
+        stones = 0;
+        updateStones(stones);
+        return removed;
     }
 
+    private void updateStones(int count) {
+        removeAll(); // Clear existing stones
+        stoneIcons.clear();
+
+        for (int i = 0; i < count; i++) {
+            JLabel stone = new JLabel();
+            stone.setPreferredSize(new Dimension(10, 10)); // Size of each stone
+            stone.setOpaque(true);
+            stone.setBackground(Color.RED);
+            stone.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            stoneIcons.add(stone);
+            add(stone);
+        }
+        revalidate();
+        repaint();
+    }
 }
